cc3fd9162a75a4516e9feec541d2134d7f72b9d5
Felipe <felipewebdesignpro@gmail.com>
2026-07-28 15:38:15 -0300

    feat: adiciona verificação caso não tenha nome

--- src/js/ui.js
+++ src/js/ui.js
@@ -24,7 +24,7 @@ export function renderProfile(userData, userRepos, container) {
     userData.name
   }" class="profile-avatar">
       <div class="profile-info">
-        <h2>${userData.name}</h2>
+        <h2>${userData.name || "Não possui nome cadastrado"}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
       </div>
     </div>
